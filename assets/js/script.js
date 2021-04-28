87f53119e6be3acae8bed9b557bd445c498f2904
Danny Roubin <dannyroubin1@gmail.com>
2021-04-27 18:16:01 -0700

    got logic to completely work, check with console log, next step will be to create content on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,41 @@
 var timeDisplayEl = $("#currentDay");
 
-
 function displayDay() {
-    $("#currentDay").text(moment().format('dddd, MMMM Do'));
-  }
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+}
+setInterval(displayDay, 1000);
 
-  console.log (displayDay( $("#currentDay")));
+var currentHour = moment().format("H");
+
+var hourObject = {
+  hourDisplayed: [9, 10, 11, 12, 1, 2, 3, 4, 5],
+  hourChecked: [9, 10, 11, 12, 13, 14, 15, 16, 17],
+};
+
+for(var i = 0; hourObject.hourChecked[i]; i++){
+
+if (hourObject.hourChecked[i] < currentHour) {
+  // create column holding hour
+  //create row with ability to type text in
+  //create save column
+  console.log("this hour has passed");
+}
+
+if (hourObject.hourChecked[i] === currentHour) {
+  // create column holding hour
+  //create row with ability to type text in
+  //create save column
+  console.log("it's " + currentHour + " right now");
+}
+
+if (hourObject.hourChecked[i] > currentHour) {
+  // create column holding hour
+  //create row with ability to type text in
+  //create save column
+  console.log("this hour has not come yet");
+}
+}
+
+console.log(currentHour);
+
+console.log(i);
